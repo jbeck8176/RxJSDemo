c132dc2133cb4588407f8524e9eb592122e20701
Jeff Beck <jbeck8176@gmail.com>
2017-01-24 23:03:19 -0500

    update

--- RXJsDemo/ClientApp/app/app.module.ts
+++ RXJsDemo/ClientApp/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './components/app/app.component'
 
 import { NavMenuComponent } from './components/navmenu/navmenu.component';
 import { HomeComponent } from './components/home/home.component';
+import { PromiseVsObservableComponent } from './components/promiseVsObservable/promiseVsObservable.component';
 import { BasicsComponent } from './components/basics/basics.component';
 import { CachingComponent } from './components/caching/caching.component';
 import { DataSharingComponent } from './components/dataSharing/dataSharing.component';
 import { DataSharingInnerComponent } from './components/dataSharingInner/dataSharingInner.component';
 import { ElementEventsComponent } from './components/elementEvents/elementEvents.component';
 import { UnittestingComponent } from './components/unittesting/unittesting.component';
+
 
 import { NameService } from './services/name.service';
 
@@ -20,6 +22,7 @@ import { NameService } from './services/name.service';
         AppComponent,
         NavMenuComponent,
         HomeComponent,
+        PromiseVsObservableComponent,
         BasicsComponent,
         CachingComponent,
         DataSharingComponent,
@@ -32,6 +35,7 @@ import { NameService } from './services/name.service';
         RouterModule.forRoot([
             { path: '', redirectTo: 'home', pathMatch: 'full' },
             { path: 'home', component: HomeComponent },
+            { path: 'promisevsobservable', component: PromiseVsObservableComponent },
             { path: 'basics', component: BasicsComponent },
             { path: 'caching', component: CachingComponent },
             { path: 'datasharing', component: DataSharingComponent },
